cb0465fdfadb4f396d1734a3de8c204ac8f0e4d0
Phantase <phantase@free.fr>
2016-09-29 23:16:15 +0200

    Add option to PDO to have real type from SQL request

--- www/mvc/models/sql_connection.php
+++ www/mvc/models/sql_connection.php
@@ -3,7 +3,7 @@
 // Database connection
 try
 {
-    $bdd = new PDO('mysql:host=db;dbname=experiments;charset=utf8', 'experiments', 'anexperimentspassword');
+    $bdd = new PDO('mysql:host=db;dbname=experiments;charset=utf8', 'experiments', 'anexperimentspassword', array(PDO::ATTR_EMULATE_PREPARES => false));
 }
 catch(Exception $e)
 {
